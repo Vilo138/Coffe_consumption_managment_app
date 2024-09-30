3f6ec3458c4421f3ccb436f0cb64148801126580
viliamvrba13@gmail.com <viliamvrba13@gmail.com>
2024-09-30 18:08:23 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,11 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.users
 
 
 class Form1(Form1Template):
@@ -8,3 +14,8 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    anvil.users.login_with_form()
+    
