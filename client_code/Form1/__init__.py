f91cb9dce49f9a53121458aa745bc892feece560
viliamvrba13@gmail.com <viliamvrba13@gmail.com>
2024-10-21 14:23:46 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,53 +1,34 @@
+from ._anvil_designer import Form1Template
+from anvil import *
 import anvil.server
 import anvil.tables as tables
 from anvil.tables import app_tables
-from ._anvil_designer import Form1Template
-from anvil import *
-import anvil.users
-import anvil.server
 
-@anvil.server.callable
-def get_users():
-    return app_tables.users.search()
-
-@anvil.server.callable
-def update_user_count(user_id):
-    user = app_tables.users.get_by_id(user_id)
-    if user:
-        user['count'] += 1
-        user.update()
-        return user
-    return None
-
-
-
-class Form1(Form1Template):
+class Form1(Form1Template):  # Trieda Form1 dedí z triedy Form1Template
     def __init__(self, **properties):
+        # Toto nastaví komponenty na formulári
         self.init_components(**properties)
 
-       
-        self.users = anvil.server.call('get_users')  
-        self.buttons = []
-        self.create_buttons()
-
-    def create_buttons(self):
-        for user in self.users:
-            button = Button(text=f"{user['name']}: {user['count']}", user=user)
-            button.role = "raised"
-            button.set_event_handler('click', self.button_click)
-            self.buttons.append(button)
-            self.add_component(button)
-
-    def button_click(self, **event_args):
-        button = event_args['sender']
-        user = button.user
-        updated_user = anvil.server.call('update_user_count', user.get_id())  
-        if updated_user:
-            button.text = f"{updated_user['name']}: {updated_user['count']}"
-
-    def link_2_click(self, **event_args):
-        anvil.users.login_with_form()
-
     def button_1_click(self, **event_args):
-      """This method is called when the button is clicked"""
-      pass
+        """Túto metódu zavolá systém pri kliknutí na tlačidlo 1"""
+        response = alert("Si si istý, že si si dal kávu?", buttons=[("Áno", True), ("Nie", False)])
+        if response:
+            print("Áno")
+        else:
+            open_form('Form1')  # Otvorí domovskú obrazovku (Form1)
+
+    def button_2_click(self, **event_args):
+        """Túto metódu zavolá systém pri kliknutí na tlačidlo 2"""
+        response = alert("Si si istý, že si si dal kávu?", buttons=[("Áno", True), ("Nie", False)])
+        if response:
+            print("Áno")
+        else:
+            open_form('Form1')  # Otvorí domovskú obrazovku (Form1)
+
+    def button_3_click(self, **event_args):
+        """Túto metódu zavolá systém pri kliknutí na tlačidlo 3"""
+        response = alert("Si si istý, že si si dal kávu?", buttons=[("Áno", True), ("Nie", False)])
+        if response:
+            print("Áno")
+        else:
+            open_form('Form1')  # Otvorí domovskú obrazovku (Form1)
