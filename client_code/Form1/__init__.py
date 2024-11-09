83d49021ff831fb3a48212e6d3a5f6ee4b80d9a8
viliamvrba13@gmail.com <viliamvrba13@gmail.com>
2024-11-09 13:54:18 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -3,13 +3,9 @@ from anvil import *
 import anvil.server
 import anvil.tables as tables
 from anvil.tables import app_tables
-
-
-
+from datetime import datetime
 from anvil.google.drive import app_files
-
 import anvil.tables.query as q
-
 import anvil.users
 
 
