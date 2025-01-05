34aa34084394bf6137c55dc3db3962c9b367731c
viliamvrba13@gmail.com <viliamvrba13@gmail.com>
2025-01-05 16:48:03 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -33,20 +33,22 @@ class Form1(Form1Template):
             # Dynamicky pridávame tlačidlo pre každého používateľa
             button = Button(text=user['email'])
             button.tag.user_id = user['id']  # Uložíme user_id do tagu tlačidla
+            button.tag.user_email = user['email']
             button.set_event_handler('click', self.zaznam_kavy)
             self.flow_panel_1.add_component(button)
 
     def zaznam_kavy(self, **event_args):
         """Pri kliknutí na tlačidlo zaznamená výber kávy do tabuľky"""
         user_id = event_args['sender'].tag.user_id  # Získame user_id z tagu tlačidla
-        print(f"User ID: {user_id}")
+        user_email = event_args['sender'].tag.user_email
+        print(f"User ID: {user_email}")
         
         # Zobrazenie dialógového okna s otázkou
-        response = alert("Si si istý, že si si dal kávu?", buttons=[("Áno", True), ("Nie", False)])
+        response = alert("Do you want to confirm this coffee log?", buttons=[("Yes", True), ("No", False)])
         if response:
             # Uloženie záznamu do tabuľky pomocou serverovej funkcie
             anvil.server.call('add_coffee_record', user_id)
-            alert(f"Zaznamenaná káva pre používateľa s ID: {user_id}")
+            alert(f"Coffee log recorded for user: {user_email}")
 
     def sign_in_button(self, **event_args):
         """Pri kliknutí na link sa používateľ prihlási"""
