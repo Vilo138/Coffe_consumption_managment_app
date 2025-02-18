f02498049e1af84f3f21b737f82492d0e4b0fc24
viliamvrba13@gmail.com <viliamvrba13@gmail.com>
2025-02-18 15:30:53 +0000

    Created DB_users.RowTemplate4, DB_users.RowTemplate5, DB_users.RowTemplate6, DB_users.RowTemplate7 and 2 more; Edited theme.css, settings, DB_users and DB_users.RowTemplate3

--- client_code/DB_users/__init__.py
+++ client_code/DB_users/__init__.py
@@ -14,15 +14,18 @@ class DB_users(DB_usersTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.repeating_panel_1.items = app_tables.users.search(tables.order_by('id'))
+    #self.repeating_panel_1.items = app_tables.users.search(tables.order_by('id'))
+    #self.repeating_panel_1.items = app_tables.users.client_writable()
+    self.repeating_panel_1.items = app_tables.users.client_writable().search(tables.order_by("id", ascending=True))
+
     self.data_grid_1.role = 'wide'
     #self.get_data()
 
     # Any code you write here will run before the form opens.
-  def get_data(self):
+  #def get_data(self):
     #db_data = 
-    self.repeating_panel_1.items = anvil.server.call("get_users_data").search()
+    #self.repeating_panel_1.items = anvil.server.call("get_users_data").search()
 
-  def save_changes(self, item, **event_args):
-        """Uloží zmeny pri úprave tabuľky"""
-        anvil.server.call('update_row', item)
+  #def save_changes(self, item, **event_args):
+  #      """Uloží zmeny pri úprave tabuľky"""
+   #     anvil.server.call('update_row', item)
