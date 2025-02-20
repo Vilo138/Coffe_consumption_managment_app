27f79e2b4e7c4c2d139e8a7960846ae7489ac79d
viliamvrba13@gmail.com <viliamvrba13@gmail.com>
2025-02-20 10:03:45 +0000

    Edited DB_users and DB_users.ItemTemplate2; Deleted DB_users.ItemTemplate1, DB_users.RowTemplate1, DB_users.RowTemplate10, DB_users.RowTemplate2 and 4 more

--- client_code/DB_users/ItemTemplate2/__init__.py
+++ client_code/DB_users/ItemTemplate2/__init__.py
@@ -8,6 +8,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from .. import DB_users
 
 
 class ItemTemplate2(ItemTemplate2Template):
@@ -17,15 +18,37 @@ class ItemTemplate2(ItemTemplate2Template):
     self.text_box_1.text = self.item['id']
     self.text_box_2.text = self.item['name']
     self.text_box_3.text = self.item['email']
+    self.text_box_2.enabled = False
+
 
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
+  def button_del_click(self, **event_args):
+    #self.repeating_panel_1.items = app_tables.users.client_writable().search(tables.order_by("id", ascending=True))
     if confirm('Are you sure you want to PERMANENTLY delete this row?'):
       self.item.delete()
       self.remove_from_parent()
+  
 
-  def text_box_2_lost_focus(self, **event_args):
-    """Uloží zmeny do databázy po úprave hodnoty v TextBoxe"""
-    self.item['name'] = self.text_box_2.text  # Aktualizácia self.item
-    anvil.server.call('update_row', self.item)
+
+  #def text_box_2_lost_focus(self, **event_args):
+  #      """Uloží zmeny do databázy po úprave hodnoty v TextBoxe"""
+  #      if self.item:  # Overíme, či máme databázový riadok
+  #          self.item['name'] = self.text_box_2.text  # Aktualizácia self.item
+  #          anvil.server.call('update_row', self.item)  # Zavoláme server na uloženie
+
+  #      self.text_box_2.enabled = False
+
+  def button_edt_click(self, **event_args):
+      self.text_box_2.enabled = True
+      self.item['name'] = self.text_box_2.text  # Aktualizácia hodnoty
+      anvil.server.call('update_row', self.item)
+      load_data()
+      #self.text_box_2_lost_focus()
+
+  
+
+  
+
+    
+      
--- client_code/DB_users/__init__.py
+++ client_code/DB_users/__init__.py
@@ -18,11 +18,13 @@ class DB_users(DB_usersTemplate):
 
   def load_data(self):
     """Načíta všetky riadky do Repeating Panel"""
-    self.repeating_panel_1.items = app_tables.users.client_writable().search(tables.order_by("id", ascending=True))
+    self.repeating_panel_1.items = app_tables.users.search(tables.order_by("id", ascending=True))
+    #self.repeating_panel_1.items = app_tables.users.client_writable().search(tables.order_by("id", ascending=True))
+
 
   def button_1_click(self, **event_args):
     """Pridanie nového riadku do databázy a obnovenie UI"""
-    app_tables.users.add_row(meno="Nový užívateľ")  # Predvolená hodnota
+    app_tables.users.add_row()  # Predvolená hodnota
     self.load_data()
 
   
